a8cc449ba63ec6011af2461f8d05185173c22ba5
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-02-03 14:21:11 +0900

    reupdate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,2 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  def hello
-    render html: "hello, world! If you can see this page or these comments, please jump this page. => http://localhost:3000/users"
-  end
 end
