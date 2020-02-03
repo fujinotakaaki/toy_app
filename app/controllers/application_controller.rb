90f7e2d7f41e9003da440175dc7d282723e8611e
藤野貴明 <fujinotakaaki@fujinotakaakinoMacBook-Pro.local>
2020-02-03 14:05:16 +0900

    add micropost scaffold

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "hello, world! If you can see this page or these comments, please jump this page. => http://localhost:3000/users"
   end
 end
